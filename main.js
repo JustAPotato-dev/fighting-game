9acca5367b638243b9507b824324db7b012f240d
Atsuma <asd463291@gmail.com>
2022-05-29 14:57:03 +0800

    Feature: Attacks

--- main.js
+++ main.js
@@ -8,21 +8,46 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = "red", offset }) {
         this.position = position;
         this.velocity = velocity;
         this.width = 50;
         this.height = 150;
         this.lastKey;
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            offset: offset,
+            width: 100,
+            height: 50,
+        };
+        this.color = color;
+        this.Jumpping;
+        this.isAttacking;
     }
 
     draw() {
-        c.fillStyle = "red";
-        c.fillRect(this.position.x, this.position.y, 50, this.height);
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        //attack box
+        if (this.isAttacking) {
+            c.fillStyle = "green";
+            c.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.width,
+                this.attackBox.height
+            );
+        }
     }
 
     update() {
         this.draw();
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
 
         if (
             !(
@@ -45,6 +70,13 @@ class Sprite {
         this.Jumpping = true;
         this.velocity.y = -20;
     }
+
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.isAttacking = false;
+        }, 100);
+    }
 }
 
 const player = new Sprite({
@@ -54,7 +86,11 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 10,
+        y: 0,
+    },
+    offset: {
+        x: 0,
+        y: 0,
     },
 });
 
@@ -67,6 +103,11 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
+        y: 0,
+    },
+    color: "blue",
+    offset: {
+        x: -50,
         y: 0,
     },
 });
@@ -87,6 +128,15 @@ const keys = {
         pressed: false,
     },
 };
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+}
 
 function animate() {
     window.requestAnimationFrame(animate);
@@ -111,6 +161,27 @@ function animate() {
     } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
         enemy.velocity.x = 5;
     }
+
+    // detect for collision
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy,
+        }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false;
+    }
+
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player,
+        }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false;
+    }
 }
 
 animate();
@@ -128,6 +199,9 @@ window.addEventListener("keydown", (event) => {
         case "w":
             player.Jump();
             break;
+        case " ":
+            player.attack();
+            break;
 
         case "ArrowRight":
             keys.ArrowRight.pressed = true;
@@ -139,6 +213,9 @@ window.addEventListener("keydown", (event) => {
             break;
         case "ArrowUp":
             enemy.Jump();
+            break;
+        case "ArrowDown":
+            enemy.attack();
             break;
     }
 });
